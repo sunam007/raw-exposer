442f256c20b9b7feca101b69a7d17009c8529c5e
sunam007 <asaduzzaman.sunam@gmail.com>
2025-01-09 22:27:34 +0600

    feat: implemented Swiper-based HeroSlider component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,15 @@
 import HeroSlider from "../components/Home/HeroSlider";
+import HeroSlider1 from "../components/Home/HeroSlider1";
 
 const Home = () => {
-    return (
-        <>
-            <div>
-                <HeroSlider/>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div>
+        {/* <HeroSlider/> */}
+        <HeroSlider1 />
+      </div>
+    </>
+  );
 };
 
 export default Home;
